ef5b933a911799e2be954ed2cd1f13f8b1e0bb5c
eric.delacruz.prod <eric.delacruz.prod@gmail.com>
2019-02-22 15:50:25 -0600

    Project Initiation

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+/*function validate(){
+    let pass1 = document.getElementById("password1").value
+    let pass2 = document.getElementById("password2").value
+    if (pass1 == pass2){
+        alert("GOOD")
+        return false
+    }
+    else{
+        alert("BAD")
+        return true
+    }
+}*/
